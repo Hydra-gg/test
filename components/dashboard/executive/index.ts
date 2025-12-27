85257f027a5486f75dacc08d82d2e502b793b275
Hydra-gg <hydra.blueprint@gmail.com>
2025-12-27 10:37:39 +0530

    Delete components directory

--- components/dashboard/executive/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Executive Dashboard Components
- * Export barrel file
- */
-
-export { KPICards } from './KPICards';
-export { RecommendationQueue } from './RecommendationQueue';
-export { WhatChangedToday } from './WhatChangedToday';
